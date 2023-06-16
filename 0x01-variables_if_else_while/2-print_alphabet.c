b2a9088c486075386a2429fc61d2783d87453b4f
ibrahim <izayed079@gmail.com>
2023-06-16 16:26:13 -0700

    frist-test

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,17 @@
+/*
+ * File: 2-print_alphabet.c
+ * Auth: ibrahim M zayed
+ */
+
 #include <stdio.h>
  
 /**
- *that prints the alphabet in lowercase.
+ *main prints the alphabet in lowercase.
  *
- * return always 0.
+ * return: always 0.
  */
-
 int main(void)
 {
-
 char letter;
 
 for(letter = 'a';letter <= 'z';letter++)
