b2160cda0146014d3edef69bcaa63f0dbd1507c9
ibrahim <izayed079@gmail.com>
2023-06-16 16:16:17 -0700

    frist-test

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+ 
+/**
+ *that prints the alphabet in lowercase.
+ *
+ * return always 0.
+ */
+
+int main(viod)
+{
+
+char letter;
+
+for(letter = 'a';letter <= 'z';letter+)
+	putchar(letter);
+
+putchar('\n')
+
+return (0);
+
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+estatment(_if_else_for_while_do-while_....)
