970de50efc6b8f11c7adcecd618941680d62d9d9
ibrahim <izayed079@gmail.com>
2023-06-16 17:25:53 -0700

    frist-test

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+/*
+ * File: 5-print_numbers.c
+ * Auth: inrahim M zayed
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int number;
+
+	for (number = '0'; numder <= '10'; number++)i
+			printf("%d", number);
+	
+	printf("\n");
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+/*
+ * File: 8-print_base16.c
+ * Auth: Ibrahim M Zayed
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbers of base 16 in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int number;
+	char letter;
+
+	for (number = 0; number < 10; number++)
+		putchar((number % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
