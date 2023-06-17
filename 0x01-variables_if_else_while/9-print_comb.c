9c8dbd4e5258b714bfab9a2ca1847525d55814fe
ibrahim <izayed079@gmail.com>
2023-06-16 18:06:38 -0700

    frist-test

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,18 +10,18 @@
  *
  * Return: Always 0.
  */
-int main(void)
-{
-	int num;
 
-	for (num = '0'; num <= '9'; num++)
+int main(void)
+{	int num = 48;
+	while (num <= 57)
 	{
-	putchar(num);	
-	if (num != '9')
-	{	
+	putchar(num);
+	if (num != 57)
+	{
 	putchar(',');
 	putchar(' ');
-	}	
+	}
+	num++;
 	}
 	putchar('\n');
 	return (0);
