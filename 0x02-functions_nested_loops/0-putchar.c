fa01dcd066721c9cf6a0edb5307a800b1c2d95a6
ibrahim <izayed079@gmail.com>
2023-06-20 09:09:34 -0700

    Done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+/*
+ * File: 0-putchar.c
+ * Auth: Ibrahim M Zayed
+ *
+ */
+
+#include "stdio.h"
+
+/**
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main()
+{
+	printf("_putchar\n");
+
+	return (0);
+}
